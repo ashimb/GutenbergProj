de9cd4134921955269d999de051c84b0a69b3168
A5h1m <ashimbera@gmail.com>
2021-07-01 21:26:59 +0530

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
+  production: true,
   //baseUrl: 'http://skunkworks.ignitesol.com:8000/', // backend server
   baseUrl: 'http://gutendex.com/'
 };
